98720c06aa9127384a291652275db4dc87188569
MERT ERDEM <merdem22@ku.edu.tr>
2024-08-23 12:32:15 +0300

    Added a plane + moveable character to the scene.

---
+++ Assets/Scripts/movementScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementScript : MonoBehaviour
+{
+
+    CharacterController controller;
+    public float movementSpeed;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    
+    void Update()
+    {
+
+        //get input.
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 toMoveVector = new Vector3(x, 0, y);
+
+        controller.Move(toMoveVector * Time.deltaTime * movementSpeed);
+
+    }
+}
